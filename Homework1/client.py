a30e42fe0a3402d6d3643969e075700565b9c95c
aralsen <asen4@binghamton.edu>
2022-04-22 15:44:27 +0300

    first commit

---
+++ Homework1/client.py
@@ -0,0 +1,39 @@
+
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65431        # The port used by the server
+
+numbers = ['', '', '']
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    packet = 'Hello, world'
+    s.sendall(packet.encode('utf-8'))
+    data = s.recv(1024).decode('utf-8')
+    print('Received:', data)
+
+    packet = '1,2'
+    s.sendall(packet.encode('utf-8'))
+
+    while True:
+        data = s.recv(1024).decode('utf-8')
+        status = data.split(' ')[0]
+        data = data[len(status)+1:]
+
+        if data and status == 'fibo':
+            print(data)
+            numbers[0:1] = data.split(',')
+            numbers[2] = int(numbers[0]) + int(numbers[1])
+            packet = str(numbers[1]) + ',' + str(numbers[2])
+            s.sendall(packet.encode('utf-8'))
+
+        if data and status == 'end':
+            print(data)
+            print('Connection is closed')
+            break
+
+        if not data:
+            pass
+
